49b405ed63cb77fea5241dff81829e8f1f9edd63
Vidi Viciansyah <viciansyah@.github.com>
2020-01-09 15:58:42 +0700

    Migrate File part 1

---
+++ app/view/emaileditor/preview.php
@@ -0,0 +1,64 @@
+<?php ?>
+
+<div class="preview-email">
+        <!-- Header -->
+        <div class="panel panel-default">
+    <div class="panel-body">
+        <button class="btn btn-default" style="border-radius: 32px;"><i class="fas fa-arrow-left"></i></button>
+        <button class="btn btn-default" style="margin-left: 10px; border-radius: 32px;"><i class="fas fa-paper-plane"></i> <strong>Test Email</strong></button>
+    </div>
+    </div>
+
+    <div class="container-fluid">
+    <div class="row">
+
+        <!-- desktop / laptop -->
+        <div class="col-lg-8 col-md-8">
+
+        <div class="laptop">
+            <div class="panel panel-default">
+            <!-- header content -->
+            <div class="panel-heading">
+                <img src="<?= BASE_URL. "assets/img/citilink-logo.png" ?>" style="width: 72px; height: 48px;" />
+            </div>
+
+            <!-- body content -->
+            <div class="panel-body" style="height: 850px;">
+                <iframe src="https://i-systemasia.com/" style="width:100%;border:none;height:100%"></iframe>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <!-- mobile -->
+        <div class="col-lg-4 col-md-4">
+
+        <div class="smartphone">
+            <div class="panel panel-default" style="border: 0;">
+
+            <!-- header content -->
+            <div class="panel-heading" style="padding: 0; height: 65px;">
+                <img src="D:\preview-page\img\mobile-view-top-bar.png" />
+            </div>
+
+            <!-- body content -->
+            <div class="panel-body" style="padding: 0; height: 460px;">
+                <iframe src="https://i-systemasia.com/" style="width:100%;border:none;height:100%"></iframe>
+            </div>
+
+            <!-- footer content -->
+            <div class="panel-footer" style="border-top: 0; padding: 0; height: 44px;">
+                <img src="D:\preview-page\img\mobile-view-bottom-bar.png" />
+            </div>
+            </div>
+        </div>
+
+
+        </div>
+
+    </div>
+    <!-- end row -->
+    </div>
+    <!-- end container -->
+</div>
+
