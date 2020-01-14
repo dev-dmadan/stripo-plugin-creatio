50e844afd3ad52c747944cdcdd8799c853bdd4a1
Romadan Saputra <dev.rarasta27@gmail.com>
2020-01-14 10:02:55 +0700

    update

--- app/configuration/route.php
+++ app/configuration/route.php
@@ -2,10 +2,10 @@
 Defined('BASE_PATH') or die(ACCESS_DENIED);
 
 /** USE THIS CODE IF YOUR PROJECT IN SUBDIRECTORY OR NOT USE SERVER DEV BUILT-IN  */
-    $base  = dirname($_SERVER['PHP_SELF']);
-    if(ltrim($base, '/')) { 
-        $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($base));
-    }
+    // $base  = dirname($_SERVER['PHP_SELF']);
+    // if(ltrim($base, '/')) { 
+    //     $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], strlen($base));
+    // }
 /** USE THIS CODE IF YOUR PROJECT IN SUBDIRECTORY OR NOT USE SERVER DEV BUILT-IN */
 
 $controller = new Request();
