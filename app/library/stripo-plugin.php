3586112b32e57b40fd3c40c83a8abdd9dc1cc45f
Romadan Saputra <dev.rarasta27@gmail.com>
2020-01-23 10:41:16 +0700

    Update stripo-plugin.php

--- app/library/stripo-plugin.php
+++ app/library/stripo-plugin.php
@@ -10,11 +10,13 @@ class Stripo {
     public $minimize;
     public $htmlFull;
 
-    private $baseURL = 'https://plugins.stripo.email/api/v1/';
+    const baseURL = 'https://plugins.stripo.email/api/v1/';
     private $uri = array(
         'auth' => 'auth',
         'compress' => 'cleaner/v1/compress'
     );
+    const defaultHtmlUrl = 'https://raw.githubusercontent.com/ardas/stripo-plugin/master/Public-Templates/Basic-Templates/Trigger%20newsletter%20mockup/Trigger%20newsletter%20mockup.html';
+    const defaultCssUrl = 'https://raw.githubusercontent.com/ardas/stripo-plugin/master/Public-Templates/Basic-Templates/Trigger%20newsletter%20mockup/Trigger%20newsletter%20mockup.css';
     private $pluginId;
     private $secretKey;
     private $success = false;
@@ -26,8 +28,8 @@ class Stripo {
 
     public function __construct() {
         $this->curl = new Curl();
-        $this->uri['auth'] = $this->baseURL . $this->uri['auth'];
-        $this->uri['compress'] = $this->baseURL . $this->uri['compress'];
+        $this->uri['auth'] = self::baseURL . $this->uri['auth'];
+        $this->uri['compress'] = self::baseURL . $this->uri['compress'];
     }
 
     /**
@@ -44,6 +46,61 @@ class Stripo {
         }
 
         return $this->token;
+    }
+
+    /**
+     * 
+     */
+    public function getDefaultTemplate() {
+        $html = $this->getDefaultHtml();
+        $css = $this->getDefaultCss();
+
+        $result['html'] = $html['success'] ? $html['response'] : '';
+        $result['css'] = $css['success'] ? $css['response'] : '';
+
+        return $result;
+    }
+
+    /**
+     * 
+     */
+    private function getDefaultHtml() {
+        $this->curl->get(self::defaultHtmlUrl);
+
+        if ($this->curl->error) {
+            $this->error['code'] = $this->curl->errorCode;
+            $this->error['message'] = $this->curl->errorMessage;
+        }
+
+        $result['response'] = $this->curl->response;
+        $result['status_code'] = $this->curl->getHttpStatusCode();
+        $result['error'] = $this->error;
+
+        $this->success = true;
+        $result['success'] = $this->success;
+
+        return $result;
+    }
+
+    /**
+     * 
+     */
+    private function getDefaultCss() {
+        $this->curl->get(self::defaultCssUrl);
+
+        if ($this->curl->error) {
+            $this->error['code'] = $this->curl->errorCode;
+            $this->error['message'] = $this->curl->errorMessage;
+        }
+
+        $result['response'] = $this->curl->response;
+        $result['status_code'] = $this->curl->getHttpStatusCode();
+        $result['error'] = $this->error;
+
+        $this->success = true;
+        $result['success'] = $this->success;
+
+        return $result;
     }
 
     /**
